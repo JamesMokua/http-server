8e0149d1550e5ef82fe7f80cec44d4d7e8334178
James Mokua <jamieedgar02@gmail.com>
2022-05-04 21:33:09 +0300

    http server created

---
+++ SimpleHTTPServer.java
@@ -0,0 +1,27 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Date;
+/**
+ * Java program to create a simple HTTP Server to demonstrate how to use
+ * ServerSocket and Socket class.
+ */
+public class SimpleHTTPServer {
+
+  public static void main(String[] args) throws IOException {
+
+    try (ServerSocket server = new ServerSocket(8080)) {
+      System.out.println("Listening for connection on port 8080 ....");
+      while (true) {
+          try (Socket socket = server.accept()) {
+              Date today = new Date();
+              String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+              socket.getOutputStream()
+                    .write(httpResponse.getBytes("UTF-8"));
+          }
+      }
+    }
+  }
+}
